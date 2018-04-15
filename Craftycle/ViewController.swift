f9109ae3938d40d6cd998c7f5bc7141b72eb2838
Thinh Vo <ducthinh2410@gmail.com>
2018-04-15 19:19:42 +0300

    Initialize project

---
+++ Craftycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Craftycle
+//
+//  Created by Thinh Vo on 15/04/2018.
+//  Copyright © 2018 Craftycle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
