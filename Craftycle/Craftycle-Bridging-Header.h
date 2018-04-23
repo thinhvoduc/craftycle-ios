cb2499852f0c76c350e73d41d763ae48296e8dfa
Thinh Vo <ducthinhducthinh2410@gmail.com>
2018-04-23 03:43:20 +0300

    Instantiate Cocoapods project

---
+++ Craftycle/Craftycle-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Craftycle-Bridging-Header.h
+//  Craftycle
+//
+//  Created by Thinh Vo on 23/04/2018.
+//  Copyright © 2018 Craftycle. All rights reserved.
+//
+
+#ifndef Craftycle_Bridging_Header_h
+#define Craftycle_Bridging_Header_h
+
+#import "DTCircularActivityIndicator/DTCircularActivityIndicator.h"
+
+#endif /* Craftycle_Bridging_Header_h */
